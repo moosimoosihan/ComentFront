6b7ee3b4e58961097e55c7604952dbc4d44f8b32
김성주 <ksj@gimseongjus-Laptop.local>
2024-03-05 19:29:43 +0900

    리액트 설정 및 헤더

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
 // TODO : 헤더 구현
-import React from 'react';
+//import React from "react"; 설정 다 끝나고나니까 이 문구가 필요가 없다네요(성주)
+import logoImg from './logoImg.png';
+
+import styles from "../styles/header.module.css";
+
 
 function Header() {
-    return <div>
-        <h1>헤더 입니다.</h1>
-    </div>;
+  return (
+    <div>
+      <div className="logo">
+        <a
+          href='/'
+        ><img className={styles.logoImg} src={logoImg} /></a>
+      </div> 
+      <div className="searchBar">
+        
+      </div>     
+      <div className="logIn">
+
+      </div>
+      <div className="logOut">
+
+      </div>
+    </div>
+  );
 }
 
 export default Header;
