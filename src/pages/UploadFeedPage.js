ed264d9f6dd400bab79dd11519f58728df0c6b88
segun <segun408@gmail.com>
2024-03-15 14:54:48 +0900

    업로드 피드 로그인시 가능하도록 구현

--- src/pages/UploadFeedPage.js
+++ src/pages/UploadFeedPage.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import useAuth from "../Auth";
 
 function UploadFeedPage() {
-    return (
-        <div>
-            <form method='post' action='http://localhost:8000/feed' >
-                <input type="hidden" name="user_id" value="1" />
-                <input type="text" name="title" />
-                <select name="category">
-                    <option value="Gamming">Gamming</option>
-                    <option value="Sports">Sports</option>
-                    <option value="Business">Business</option>
-                    <option value="Crypto">Crypto</option>
-                    <option value="Television">Television</option>
-                    <option value="Celebrity">Celebrity</option>
-                </select>
-                <input type="textarea" name="content" />
-                <input type="submit" value="업로드" />
-            </form>
-        </div>
-    );
+    const isLoggedIn = useAuth();
+    const gotoHome = () => {
+        window.location.href = '/';
+    }
+    if(isLoggedIn){
+        return (
+            <div>
+                <form method='post' action='http://localhost:8000/feed' >
+                    <input type="hidden" name="user_id" value="1" />
+                    <input type="text" name="title" />
+                    <select name="category">
+                        <option value="Gamming">Gamming</option>
+                        <option value="Sports">Sports</option>
+                        <option value="Business">Business</option>
+                        <option value="Crypto">Crypto</option>
+                        <option value="Television">Television</option>
+                        <option value="Celebrity">Celebrity</option>
+                    </select>
+                    <input type="textarea" name="content" />
+                    <input type="submit" value="업로드" />
+                </form>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>로그인이 필요합니다.</h1>
+                <button onClick={gotoHome}>홈으로</button>
+            </div>
+        );
+    }
 }
 
 export default UploadFeedPage;
