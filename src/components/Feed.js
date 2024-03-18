61f7a2a161cef11ab64676be95d3268ee400a96f
segun <segun408@gmail.com>
2024-03-18 14:06:09 +0900

    피드 수정 삭제 예외처리 구현

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,16 +1,43 @@
-// TODO : 피드 구현
-import React from 'react';
 import style from '../styles/Feed.module.css';
 import { PropTypes } from 'prop-types';
+import useAuth from "../Auth";
+import axios from 'axios';
 
 function Feed(props) {
     Feed.propTypes = {
+        feed_id: PropTypes.string.isRequired,
         nickname: PropTypes.string.isRequired,
         content: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
+        user_id: PropTypes.string.isRequired
+    }
+    
+    let user = null;
+    const isLoggedIn = useAuth();
+    if(isLoggedIn){
+        user = JSON.parse(sessionStorage.getItem('userinfo') || '{}');
     }
 
-    return <>
+    async function deleteFeed(){
+        // 삭제 여부 확인
+        if(!window.confirm('정말 삭제하시겠습니까?')){
+            return;
+        }
+         try{
+            const response = await axios.post(`http://localhost:8000/feed/${props.feed_id}/delete`);
+            // 삭제 성공시 페이지 리로드
+            if(response.data){
+                window.location.reload();
+            }
+         }catch(e){
+            console.log(e);
+        }
+    }
+    function editFeed(){
+        window.location.href = `/editFeed/${props.feed_id}`;
+    }
+
+    return (<>
         <div className={style.feed_container}>
             <div className={style.feed}>
                 <p className={style.username}>{props.nickname} ㆍ 1days ago</p>
@@ -22,9 +49,15 @@ function Feed(props) {
                     <button className={style.down}>down</button>
                     <button className={style.comment}>1.4k</button>
                 </div>
+                {user && user._id === props.user_id ? (
+                    <div>
+                        <button className={style.delete} onClick={deleteFeed}>삭제</button>
+                        <button className={style.edit} onClick={editFeed}>수정</button>
+                    </div>
+                ) : null}
             </div>
         </div>
-    </>;
+    </>);
 }
 
 export default Feed;
--- src/pages/UploadFeedPage.js
+++ src/pages/UploadFeedPage.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import useAuth from "../Auth";
+import axios from 'axios';
 
 function UploadFeedPage() {
     const isLoggedIn = useAuth();
@@ -17,24 +19,63 @@ function UploadFeedPage() {
             return;
         }
     }
+
+    // 파라미터 값으로 feed_id가 있다면 수정하도록 하고, 없다면 새로운 피드를 업로드하도록 한다.
+    let [edit, setEdit] = useState(false);
+    let [feed, setFeed] = useState(undefined);
+    useEffect(()=>{
+        // 먼저 파라미터 값 여부 확인
+        if(window.location.pathname.split('/').length > 2 && !edit){
+            // 수정
+            try{
+                const feed_id = window.location.pathname.split('/')[2];
+                const fetchData = async()=>{
+                    const response = await axios.get(`http://localhost:8000/feed/${feed_id}`);
+                    setFeed(response.data);
+                };
+                fetchData();
+                setEdit(true);
+            }catch(e){
+                console.log(e);
+            }
+        }
+    }, [])
+
     const a = JSON.parse(sessionStorage.getItem('userinfo') || '{}');
     if(isLoggedIn){
-        return (
+        return (edit) ? ((feed !== undefined && a._id === feed.user_id._id) ? (
             <div>
-                <form method='post' action='http://localhost:8000/feed' onSubmit={submitFeed} >
+                <form method='post' action={`http://localhost:8000/feed/${feed._id}`} onSubmit={submitFeed} >
                     <input type="hidden" name="user_id" value={a._id} />
-                    <input type="text" name="title" />
-                    <select name="category">
-                        <option value="Gaming">Gaming</option>
-                        <option value="Sports">Sports</option>
-                        <option value="Business">Business</option>
-                        <option value="Crypto">Crypto</option>
-                        <option value="Television">Television</option>
-                        <option value="Celebrity">Celebrity</option>
+                    <input type="text" name="title" value={feed.title} onChange={(e) => setFeed({ ...feed, title: e.target.value })} />
+                    <select name="category" value={feed.category}>
+                        <option value={feed.category}>{feed.category}</option>
                     </select>
-                    <input type="textarea" name="content" />
-                    <input type="submit" value="업로드" />
+                    <input type="textarea" name="content" value={feed.content} onChange={(e) => setFeed({ ...feed, content: e.target.value })} />
+                    <input type="submit" value="수정하기" />
                 </form>
+            </div>
+        ):(
+            <div>
+            <h1>본인이 작성한 글만 수정할 수 있습니다.</h1>
+            <button onClick={gotoHome}>홈으로</button>
+            </div>
+        )):(
+            <div>
+            <form method='post' action='http://localhost:8000/feed' onSubmit={submitFeed} >
+                <input type="hidden" name="user_id" value={a._id} />
+                <input type="text" name="title" />
+                <select name="category" >
+                <option value="Gaming">Gaming</option>
+                <option value="Sports">Sports</option>
+                <option value="Business">Business</option>
+                <option value="Crypto">Crypto</option>
+                <option value="Television">Television</option>
+                <option value="Celebrity">Celebrity</option>
+                </select>
+                <input type="textarea" name="content" />
+                <input type="submit" value="업로드" />
+            </form>
             </div>
         );
     } else {
