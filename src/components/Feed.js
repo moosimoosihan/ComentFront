1f9532367a31819d871fcda693d7fb4bfccdc9c0
jiwon12121 <roboting08@naver.com>
2024-03-20 14:39:41 +0900

    댓글 기능 구현

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,6 +6,7 @@ import { BiEdit } from "react-icons/bi";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import Like from './Like';
+import { useState } from 'react';
 
 function Feed(props) {
     Feed.propTypes = {
@@ -15,32 +16,73 @@ function Feed(props) {
         title: PropTypes.string.isRequired,
         user_id: PropTypes.string.isRequired
     }
-    
+
+    const [commentView, setCommentView] = useState(false);
+    const [comment, setComment] = useState({
+        comment: "",
+    });
+
+    const commentChange = (e) => {
+        setComment({
+            ...comment,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const commentSubmit = async (e) => {
+        e.preventDefault();
+
+        const commentData = {
+            comment: comment.comment,
+        };
+
+        await axios
+            .post("http://localhost:8000/comment", commentData)
+            .then((response) => {
+                if(response.status === 201){
+                    alert("comment upload success");
+                }
+            })
+            .catch(() => {
+                alert("comment upload failed");
+            });
+    }
+
+    const commentClick = () => {
+        const commentBtn = document.querySelector('.comment');
+        if (commentView === false) {
+            setCommentView(true);
+        } else {
+            setCommentView(false);
+        }
+    };
+
     let user = null;
     const isLoggedIn = useAuth();
-    if(isLoggedIn){
+    if (isLoggedIn) {
         user = JSON.parse(sessionStorage.getItem('userinfo') || '{}');
     }
 
-    async function deleteFeed(){
+
+    async function deleteFeed() {
         // 삭제 여부 확인
-        if(!window.confirm('정말 삭제하시겠습니까?')){
+        if (!window.confirm('정말 삭제하시겠습니까?')) {
             return;
         }
-         try{
+        try {
             const response = await axios.post(`http://localhost:8000/feed/${props.feed_id}/delete`);
             // 삭제 성공시 페이지 리로드
-            if(response.data){
+            if (response.data) {
                 window.location.reload();
             }
-         }catch(e){
+        } catch (e) {
             console.log(e);
         }
     }
-    function editFeed(){
+    function editFeed() {
         window.location.href = `/editFeed/${props.feed_id}`;
     }
-    
+
 
     return (<>
         <div className={style.feed_container}>
@@ -53,13 +95,19 @@ function Feed(props) {
                     <div className={style.like_box}>
                         <Like key={props.feed_id} feed_id={props.feed_id} isLoggedIn={isLoggedIn} />
                     </div>
-                    <a className={style.comment}><FaRegCommentAlt />1.4k</a>
+                    <a className={style.comment} onClick={commentClick}><FaRegCommentAlt />1.4k</a>
                     {user && user._id === props.user_id ? (
                         <div className={style.deledit}>
                             <button className={style.delete} onClick={deleteFeed}><MdDeleteForever /></button>
                             <button className={style.edit} onClick={editFeed}><BiEdit /></button>
                         </div>
                     ) : null}
+                </div>
+                <div style={{ display: commentView ? 'block' : 'none'}}>
+                    <form onSubmit={commentSubmit}>
+                        <input type="text" value={comment.comment} onChange={commentChange} name='comment' className={style.comment_box}></input>
+                        <input type="submit" value="작성"></input>
+                    </form>
                 </div>
             </div>
         </div>
